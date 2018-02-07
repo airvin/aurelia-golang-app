94a63d629d6757e4a6b9c461fc77eeedc00e6188
Allison Irvin <allison.irvin2@gmail.com>
2018-02-07 22:18:19 +1100

    timer function working in aurelia

--- aurelia-test/src/app.js
+++ aurelia-test/src/app.js
@@ -1,10 +1,11 @@
 import {HttpClient, json} from  'aurelia-fetch-client';
-import {inject} from 'aurelia-framework';
+import {TaskQueue, inject} from 'aurelia-framework';
 
-@inject(HttpClient)
+@inject(HttpClient, TaskQueue)
 
 export class App {
-  constructor(http) {
+  
+  constructor(http,taskQueue) {
 
     http.configure(config => {
       config
@@ -15,18 +16,39 @@ export class App {
     });
 
     this.http = http;
+    this.taskQueue = taskQueue;
     this.message = 'Aurelia GoLang application';
+    this.count = 0;
+    this.updateTime(this.count);
   }
 
-  clickButton() {
-    // http://jsonplaceholder.typicode.com/posts/1
-    // http://localhost:12345/getdata
-    this.http.fetch('http://localhost:12345/getdata')
+  updateTime(count) {
+    if (this.count<10) {
+      this.count = parseInt(count)+1;
+    } else {
+      this.http.fetch('http://localhost:3000/getdata')
       .then(response => response.json())
       .then(data => {
         console.log(data.Name);
         this.message = data.Name;
       })
-  
+      this.count = 0;
+    }
+    this.taskQueue.queueMicroTask(() => {
+      console.log("count is: " + this.count);
+      window.setTimeout(() => {
+        this.updateTime(this.count)
+      },2000);
+    });
   }
+
+  clickButton() {
+    this.http.fetch('http://localhost:3000/getdata')
+      .then(response => response.json())
+      .then(data => {
+        console.log(data.Name);
+        this.message = data.Name;
+      })
+  }
+
 }
--- golang-test/main.go
+++ golang-test/main.go
@@ -1,29 +1,33 @@
 package main
 
 import (
-	"fmt"
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
-	"github.com/gorilla/mux"
+	"strconv"
+
 	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
 )
 
 func main() {
 	fmt.Println("main function called")
 	router := mux.NewRouter()
-	router.HandleFunc("/getdata",GetData)
-	log.Fatal(http.ListenAndServe(":12345", handlers.CORS(handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"}), handlers.AllowedMethods([]string{"GET", "POST", "PUT", "HEAD", "OPTIONS"}), handlers.AllowedOrigins([]string{"*"}))(router)))}
+	router.HandleFunc("/getdata", GetData)
+	log.Fatal(http.ListenAndServe(":3000", handlers.CORS(handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"}), handlers.AllowedMethods([]string{"GET", "POST", "PUT", "HEAD", "OPTIONS"}), handlers.AllowedOrigins([]string{"*"}))(router)))
+	count := 0
+	fmt.Println(count)
+}
 
 func GetData(w http.ResponseWriter, req *http.Request) {
 	type Question struct {
 		Name string
 	}
-	title := Question{"Data from the Golang API"}
-	message, err := json.Marshal(title)
+	title := Question{"Data from the Golang API" + strconv.Itoa(count)}
+
 	fmt.Println("GetData function called")
-	fmt.Println(message)
-	fmt.Println(err)
+
 	json.NewEncoder(w).Encode(title)
 	return
 }
